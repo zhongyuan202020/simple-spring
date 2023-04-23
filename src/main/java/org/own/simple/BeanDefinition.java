863a964e089a05b570263ac6139fd2af5c2d2cb5
zhao.zhongyuan <zhaozhongyuan@zhaozhongyuandeMacBook-Pro.local>
2023-04-23 10:32:18 +0800

    init

---
+++ src/main/java/org/own/simple/BeanDefinition.java
@@ -0,0 +1,35 @@
+package org.own.simple;
+
+/**
+ * BeanDefinition
+ *
+ * @author zhaozhongyuan
+ * @date 2023/4/23
+ */
+public class BeanDefinition {
+
+    private String id;
+
+    private String className;
+
+    public BeanDefinition(String id, String className) {
+        this.id = id;
+        this.className = className;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+}
